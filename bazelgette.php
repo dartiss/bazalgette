453dc4ee445b65a5b94493e1aedf1fe19a72024c
David Artiss <david.artiss@automattic.com>
2020-04-13 08:02:46 +0100

    Initial release

---
+++ bazelgette.php
@@ -0,0 +1,108 @@
+<?php
+/**
+Plugin Name: Bazalgette
+Plugin URI: https://wordpress.org/plugins/bazalgette
+Description: Automatically tidy up your cluttered admin menus
+Version: 0.1
+Author: David Artiss
+Author URI: https://artiss.blog
+Text Domain: bazalgette
+
+@package bazalgette
+ */
+
+/**
+ * THIS VERSION IS AN EARLY PROOF OF CONCEPT - IT WORKS BUT THERE ARE NO OPTIONS AND IT DOES
+ * JUST A BASIC CLEAN
+ */
+
+/**
+ * Add meta to plugin details
+ *
+ * Add options to plugin meta line
+ *
+ * @param  string $links  Current links.
+ * @param  string $file   File in use.
+ * @return string         Links, now with settings added.
+ */
+function bazalgette_plugin_meta( $links, $file ) {
+
+	if ( false !== strpos( $file, 'tidy-my-menus.php' ) ) {
+
+		$links = array_merge( $links, array( '<a href="https://github.com/dartiss/bazalgette">' . __( 'Github', 'bazalgette' ) . '</a>' ) );
+
+		$links = array_merge( $links, array( '<a href="https://wordpress.org/support/plugin/bazalgette">' . __( 'Support', 'bazalgette' ) . '</a>' ) );
+	}
+
+	return $links;
+}
+
+add_filter( 'plugin_row_meta', 'bazalgette_plugin_meta', 10, 2 );
+
+/**
+ * Tidy the menus
+ *
+ * Main code to automatically move and remove items from the admin menus
+ */
+function bazalgette() {
+
+	$menus    = $GLOBALS['menu'];
+	$submenus = $GLOBALS['submenu'];
+
+	// Exclusion array - none of the menu options in this list will be touched by the automated removal process.
+
+	$exclusion_array = array( 'bp-activity', 'edit-comments.php', 'galleries_bwg' );
+
+	// Special array - move specific sub-menus to settings or just delete the menu altogether.
+
+	$special_array = array(
+		'gutenberg'  => '',
+		'metaslider' => 'MetaSlider',
+		'wppr'       => 'Product Review',
+	);
+
+	// Process through the current menu structure, moving and removing menus according to the above array and some simple logic that a menu option
+	// with no sub-menus probably belongs in 'settings'.
+
+	foreach ( $menus as $main_menu_array ) {
+
+		$submenu = $submenus[ $main_menu_array[2] ];
+
+		if ( 0 === count( $submenu ) && ! in_array( $main_menu_array[2], $exclusion_array, true ) && '' !== $main_menu_array[0] ) {
+
+			// Remove the menu option.
+			remove_menu_page( $main_menu_array[2] );
+
+			// Work out the correct page name.
+			$page = $main_menu_array[5];
+			if ( 'toplevel_page_' === substr( $page, 0, 14 ) ) {
+				$page = substr( $page, 14 );
+			}
+
+			// Now add menu option as a sub-menu to settings.
+			add_submenu_page( 'options-general.php', $main_menu_array[3], $main_menu_array[0], $main_menu_array[1], $page, $main_menu_array[6] );
+		}
+
+		// If this is a standard menu and it matches the main menu slug, process it.
+		if ( isset( $submenu ) && array_key_exists( $main_menu_array[2], $special_array ) ) {
+
+			$submenu_page = $special_array[ $main_menu_array[2] ];
+
+			// Remove the main menu option.
+			remove_menu_page( $main_menu_array[2] );
+
+			// If the requested submenu page is blank, that means I just want to delete all the menus out and not move anything.
+			if ( '' !== $submenu_page ) {
+
+				// Now look for the relevant sub-menu and move it to the settings.
+				foreach ( $submenu as $sub_menu_array ) {
+					if ( $sub_menu_array[0] === $submenu_page ) {
+						add_submenu_page( 'options-general.php', $sub_menu_array[3], $sub_menu_array[0], $sub_menu_array[1], $sub_menu_array[2] );
+					}
+				}
+			}
+		}
+	}
+}
+
+add_action( 'admin_init', 'bazalgette' );
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== Bazalgette ===
+Contributors: dartiss
+Tags: menu, admin, tidy, clean, housekeep
+Requires at least: 4.6
+Tested up to: 5.4
+Requires PHP: 5.3
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Automatically tidy up your cluttered admin menus.
+
+== Description ==
+
+Are you tired of all those menus that plugins add to your admin screen? Noticed how many just link to a settings page that really should have been under the 'Settings' menu? Well, tire no more - Bazalgette will do all of this for you.
+
+1. If a menu has no sub-menus, it will automatically move it to the Settings menu.
+2. Known plugins that create sub-menus but, still, only one is relevant, will be identified and their settings moved to the right place.
+3. If a plugin adds a menu option that adds no useful links at all, it'll be removed!
+
+The latter 2 options are reliant on YOU telling ME about plugins that you'd like added. I've already tested with all your favorites (far too many to count but, needless to say, I had one cluttered dashboard)! If there's something you'd like me to add please see the FAQ for more details.
+
+Equally, please get in touch (again, see the FAQ) if there's any feature you'd like to see added.
+
+Here's the science...
+
+* Designed for both single and multi-site installations
+* PHP7 compatible
+* Passes [WordPress.com VIP](https://vip.wordpress.com) coding standards and fully compatible with their platform
+* Fully internationalized, ready for translations **If you would like to add a translation to his plugin then please head to our [Translating WordPress](https://translate.wordpress.org/projects/wp-plugins/bazalgette "Translating WordPress") page**
+* Gutenberg ready
+* GDPR compliant
+
+Please visit the [Github page](https://github.com/dartiss/bazalgette) for the latest code development, planned enhancements and known issues.
+
+== Installation ==
+
+Tidy My Menus can be found and installed via the Plugin menu within WordPress administration (Plugins -> Add New). Alternatively, it can be downloaded from WordPress.org and installed manually...
+
+1. Upload the entire `bazalgette s` folder to your `wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress administration.
+
+Voila! It's ready to go.
+
+== Frequently Asked Questions ==
+
+= Why is this called Bazalgette? =
+
+Sir Joseph Bazalgette was a 19th century engineer who created London's first sewer systems. Called into action due to the terrible smell from the polluted River Thames, his actions literally helped to "clean up the crap". And that's what this plugin does.
+
+= Hey! This is hiding something it shouldn't / not hiding something it should / I'd like to suggest a funky new feature =
+
+Feedback is everything! Please get in touch and let me know, either via the [WordPress.org forum](https://wordpress.org/support/plugin/bazalgette) or our [Github repo](https://github.com/dartiss/bazalgette).
+
+== Changelog ==
+
+[Learn more about my version numbering methodology](https://artiss.blog/2016/09/wordpress-plugin-versioning/ "WordPress Plugin Versioning")
+
+= 0.1 =
+* Initial release.
+
+== Upgrade Notice ==
+
+= 0.1 =
+* Initial release.
